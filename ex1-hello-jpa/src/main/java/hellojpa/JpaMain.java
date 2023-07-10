6d2b4f89a46f72a75eeef17af4e2cdc018e75d3e
MyFinale <lunar7s@naver.com>
2023-07-11 01:37:06 +0900

    23.07.11

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,11 +1,87 @@
 package hellojpa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.sound.midi.Soundbank;
+import java.util.List;
 
 public class JpaMain {
 
     public static void main(String[] args) {
-        Persistence.createEntityManagerFactory("hello");
+        /**
+         * 엔티티 매니저 팩토리는 하나만 생성해서 애플리케이션 전체에서 공유
+         *
+         * 엔티티 매니저는 쓰레드간 공유하지 않는다.
+         *
+         * JPA의 모든 데이터 변경은 트랜잭션 안에서 실행
+         */
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("HelloB");
+
+//            Member findMember = em.find(Member.class, 1L);
+//            System.out.println("findMember.id = " + findMember.getId());
+//            System.out.println("findMember.name = " + findMember.getName());
+
+//            em.remove(findMember);
+
+//            findMember.setName("HelloJPA");
+//            em.persist(findMember); 수정 시 이 코드는 필요가 없음
+
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+////                    .setFirstResult(1)
+////                    .setMaxResults(4)
+//                    .getResultList();
+
+//            for (Member member : result) {
+//                System.out.println("member.name = " + member.getName());
+//            }
+
+//            // 비영속
+//            Member member = new Member();
+//            member.setId(100L);
+//            member.setName("HelloJPA");
+//
+//            // 영속
+//            System.out.println("=== BEFORE ===");
+//            em.persist(member); // 이 시점에 쿼리가 실행되지 않음  em.flush(); 를 이용하면 그 즉시 쿼리 실행
+////            em.detach(member);
+////            em.remove(member);
+//            System.out.println("=== AFTER ===");
+//
+//            Member findMember = em.find(Member.class, 100L);
+//            System.out.println("findMember.id = " + findMember.getId());
+//            System.out.println("findMember.name = " + findMember.getName());    // DB를 조회하지 않고 1차 캐시에서 조회 (line 56에 의해)
+
+//            Member f1 = em.find(Member.class, 1L);
+//            Member f2 = em.find(Member.class, 1L);  // 쿼리문은 한번만 실행됨 -> 1차 캐시에서 조회
+
+//            System.out.println("동일성 보장 여부 : " + (f1 == f2));
+
+            Member member = new Member();
+//            member.setId(3L);
+            member.setUsername("C");
+            member.setRoleType(RoleType.USER);
+
+            em.persist(member);
+
+            tx.commit();    // 이 시점에 쿼리가 실행됨
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
 
     }
 }
---
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -0,0 +1,91 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+//@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
+    private Long id;
+
+    @Column(name = "name")
+    private String username;
+
+    private Integer age;
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    private LocalDate testLocalDate;
+    private LocalDateTime testLocalDateTime;
+    @Lob
+    private String description;
+
+    public Member() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
