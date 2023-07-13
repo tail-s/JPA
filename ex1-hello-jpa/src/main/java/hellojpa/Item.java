55597af83cb31247ede8e8d91dc54b80e6f0e900
MyFinale <lunar7s@naver.com>
2023-07-14 03:04:02 +0900

    strategy = InheritanceType.SINGLE_TABLE

--- ex1-hello-jpa/src/main/java/hellojpa/Item.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Item.java
@@ -3,8 +3,8 @@ package hellojpa;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
-@DiscriminatorColumn(name = "category")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "category") // 단일 테이블 전략에서 이 코드를 안써도 DTYPE이 자동으로 들어감
 public class Item {
 
     @Id
