b77f683c00331def25e7c7e3ea5d0ede7d34465a
MyFinale <lunar7s@naver.com>
2023-07-17 23:36:53 +0900

    JPQL

--- JPQL/src/main/java/jpql/JpaMain.java
+++ JPQL/src/main/java/jpql/JpaMain.java
@@ -19,11 +19,23 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
-            List<Member> resultList = query1.getResultList();
-            for (Member m : resultList) {
-                System.out.println("member = " + m);
-            }
+//            TypedQuery<Member> query1 = em.createQuery("select m from Member m where m.id = 10", Member.class);
+
+//            TypedQuery<Member> query1 = em.createQuery("select m from Member m where m.username = :username", Member.class);
+//            query1.setParameter("username", "member1");
+//            Member singleResult = query1.getSingleResult(); // 결과가 없거나 둘 이상일 때 예외 발생
+
+            Member singleResult = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                            .setParameter("username", "member1")
+                            .getSingleResult();
+
+
+            System.out.println("singleResult = " + singleResult);
+
+//            List<Member> resultList = query1.getResultList();
+//            for (Member m : resultList) {
+//                System.out.println("member = " + m);
+//            }
 
             TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
             Query query3 = em.createQuery("select m.username, m.age from Member m");
